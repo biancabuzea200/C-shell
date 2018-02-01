8b474aca3ce78b87e190094fad77d5fec30dffb6
Aleksi <aleksi.daskalov.2016@uni.strath.ac.uk>
2018-02-01 16:41:54 +0000

    Added checks for command length and included all deliminator

---
+++ shell.c
@@ -0,0 +1,43 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void readInput();
+
+int main () {
+readInput();
+return 0;
+}
+
+/*
+Reads and parses the user input in tokens.
+*/
+void readInput(){
+
+    char input[512] = "";
+    const char d[12] = " \t<>|;&";
+    char *token = "";
+    const char check[6] = "exit\n";
+   while(1)
+   {
+      printf("$");
+
+      fgets(input,512,stdin);
+        if(strcmp(check,input) == 0){
+            break;
+        }
+        if(input[strlen(input)-1]!= '\n'){
+            printf("The command is too long!\n");
+            input[0] = "";
+            }
+
+      token = strtok(input, d);
+
+      while( token != NULL )
+      {
+        printf("%s\n", token );
+        token = strtok(NULL, d);
+      }
+   }
+}
+
