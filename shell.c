7ee70f2e947fbc4ff6288ddea6ceae374141c9de
Rumen Hristianov Stoykov CES2016 <rgb16151@baird-12.cis.strath.ac.uk>
2018-02-02 16:33:20 +0000

    Part 1 finished and fully tested

--- shell.c
+++ shell.c
@@ -2,42 +2,64 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void readInput();
+#define MAXCHAR 512
+char* readInput(char tokens[MAXCHAR]);
 
-int main () {
-readInput();
+int main () 
+{
+char input[512] = "";
+
+//Tests if the tokens have been stored correctly... 
+	
+		printf("%s",readInput(input));
+
 return 0;
 }
 
 /*
-Reads and parses the user input in tokens.
+Reads,parses the user input and returns a char pointer to the tokens array.
+Parameters: char tokens[MAXCHAR]
 */
-void readInput(){
+char* readInput(char tokens[MAXCHAR]){
 
-    char input[512] = "";
-    const char d[12] = " \t<>|;&";
+    char input[MAXCHAR] = "";
+    const char d[13] = " \t<>|;&\n";
     char *token = "";
     const char check[6] = "exit\n";
+    char c,systemsymbol = '$';
    while(1)
    {
-      printf("$");
+      printf("%c",systemsymbol);
 
-      fgets(input,512,stdin);
-        if(strcmp(check,input) == 0){
+      fgets(input,MAXCHAR,stdin);
+	//Checks if "exit" or Ctrl + D are present in the input...
+        if((strcmp(check,input) == 0) || feof(stdin) )
+	{
             break;
         }
-        if(input[strlen(input)-1]!= '\n'){
-            printf("The command is too long!\n");
-            input[0] = "";
-            }
+	//Checks if the input is more than 512 symbols...        
+	if(input[strlen(input)-1] != '\n')
+	{
+            printf("%cThe command is too long!\n",systemsymbol);  
+	    //The following line flushes the buffer after the 512th symbol...
+	    while ((c = getchar()) != '\n' && c != EOF) {}
+            strcpy(input,"");
+        }
 
       token = strtok(input, d);
-
+      
       while( token != NULL )
       {
-        printf("%s\n", token );
-        token = strtok(NULL, d);
+	if (strcmp(token,"\n") != 0)
+	{
+		
+			strcat(tokens,token);
+		
+	printf("%s\n", token );
+	}
+      token = strtok(NULL, d);
       }
    }
+return tokens;
 }
 
