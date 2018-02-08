3b1f09f3b1035402ae5d79f688a85e5da2e85069
Rumen Hristianov Stoykov CES2016 <rgb16151@baird-06.cis.strath.ac.uk>
2018-02-08 20:22:07 +0000

    parts 1 & 2 done part 3 almost done

---
+++ parts123.c
@@ -0,0 +1,222 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+
+#define MAXCHAR 512
+
+char **readInput(char **tokens);
+void setToHome();
+void setPath(char* token);
+void getPath();
+void execute_command(char** tokens,char *initPath);
+int getNumberOfTokens(char **tokens);
+
+int main()
+{
+	char **input;
+	char *path;
+	
+	path = strdup(getenv("PATH"));
+	// Print PATH environment variable
+	printf("PATH IS -> %s\n", path);
+	
+	setToHome();
+	input = readInput(input);
+	
+	return 0;
+}
+
+/*
+Reads,parses the user input and returns a char pointer to the tokens array.
+Parameters: char tokens[MAXCHAR]
+*/
+char **readInput(char **tokens)
+{
+	int token_cnt = 0; 
+	char input[MAXCHAR] = "";
+	const char delimiters[13] = " \t<>|;&\n";
+	char *token = "";
+	const char exit_command[6] = "exit\n";
+	char c, systemsymbol = '$';
+	char *path;
+
+	tokens = (char **)malloc(50 * sizeof(char *));
+	printf("Tokens array successfully created!\n");
+
+	path = strdup(getenv("PATH"));
+	// Print PATH environment variable
+	printf("PATH IS -> %s\n", path);
+
+	while (1)
+	{
+		
+		token_cnt = 0;			
+		printf("%c", systemsymbol);
+
+		fgets(input, MAXCHAR, stdin);
+		
+		//Checks if "exit" or Ctrl + D are present in the input...
+		if (feof(stdin))
+		{
+			setPath(path);
+			printf("\n");			
+			exit(0);
+		}
+
+		//Checks if the input is more than 512 symbols...
+		if (input[strlen(input) - 1] != '\n')
+		{
+			printf("The command is too long!\n");
+			printf("%c", systemsymbol);
+			//The following line flushes the buffer after the 512th symbol...
+			while ((c = getchar()) != '\n' && c != EOF)
+			{
+			}
+			strcpy(input, "");
+		}
+
+		token = strtok(input, delimiters);
+		
+		while (token != NULL)
+		{
+			if (strcmp(token, "\n") != 0)
+			{
+				tokens[token_cnt]=strdup(token);
+				printf("COPYING '%s' into tokens[%d]\n", token, token_cnt);
+				token_cnt++;
+			}
+			token = strtok(NULL, delimiters);
+		}
+		tokens[token_cnt] = NULL;
+		
+		if(tokens[0] != NULL)
+		{		
+			execute_command(tokens,path);			
+		}
+	}
+	return tokens;
+}
+/*
+The following function sets the Environment variable HOME . 
+Parameters: none.
+*/
+void setToHome()
+{
+char current_dir[100];
+if (chdir(getenv("HOME")) == 0)
+	{
+		printf("CURRENT DIRECTORY SUCCESSFULLY CHANGED !\n");		
+		getcwd(current_dir,100);
+		printf("CURRENT DIRECTORY IS -> %s\n", current_dir);	
+	}
+	else
+	printf("CURRENT DIRECTORY HASN'T BEEN CHANGED!");
+}
+/*
+The following function returns the Environment variable PATH. 
+Parameters: none.
+*/
+void getPath()
+{
+	printf("%s\n",getenv("PATH"));
+}
+/*
+The following function sets the Environment variable PATH. 
+Parameters: char* token.
+*/
+void setPath(char* token)
+{	
+	struct stat buffer;
+	int status;
+	
+	status = lstat(token, &buffer);
+
+	if (status != 0)
+	{
+		printf("Invalid path!\n");	
+	}
+	else if (setenv("PATH",token,1) != 0)
+	{
+		printf("Error while setting the path!\n");
+	}
+}
+/*
+The following function executes the commands. 
+Parameters: char** tokens and char*initPath.
+*/
+void execute_command(char** tokens,char *initPath)
+{
+pid_t pid;
+int pid_status;
+int exec_status;	
+
+		if (strcmp(tokens[0],"getpath") == 0) 
+		{			
+			getPath();
+			
+		}
+		else if (strcmp(tokens[0],"setpath") == 0)
+		{
+		 if(getNumberOfTokens(tokens) == 2 ){
+			setPath(tokens[1]);		
+			}
+		else {
+			printf("Error! Invalid number of arguments for 'setpath'.\n");
+			}
+		}
+		else if (strcmp(tokens[0], "exit") == 0)
+		{
+			setPath(initPath);
+			
+			exit(0);
+		}		
+		else
+		{
+			pid = fork();
+
+			// Fork a child process
+			if (pid < 0)
+			{
+				// Fork error
+				printf("Fork Failed\n");
+				setPath(initPath);
+				
+				exit(1);
+			}
+			else if (pid == 0)
+			{
+				// Child process
+				exec_status = execvp(tokens[0], tokens);
+				if (exec_status < 0)
+				{
+				perror(tokens[0]);
+				exit(2);
+				}
+			}
+			else
+			{ 	
+				// Wait for completition
+				while (wait(&pid_status) != pid);
+			}
+		}
+	
+}
+/*
+The following function returns the number of tokens entered per line.
+Parameters: char** tokens
+*/
+int getNumberOfTokens(char** tokens)
+{
+	int i = 0, cnt = 0;
+
+	while (tokens[i] != NULL)
+	{
+		i++;
+		cnt++;
+	}
+	return cnt;
+}
